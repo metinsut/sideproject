14d218b56abcecb9ec7ee553bb02350da22bb6b3
Metin Süt <metindir@gmail.com>
2026-01-01 21:21:46 +0300

    update project-card

--- src/routes/app/project/-project/index.tsx
+++ src/routes/app/project/-project/index.tsx
@@ -1,57 +1,15 @@
-import { useLoaderData, useNavigate } from "@tanstack/react-router";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { useChangeProject } from "@/lib/queries/projects";
-import { cn } from "@/lib/utils";
+import { useLoaderData } from "@tanstack/react-router";
+import { ProjectCard } from "./project-card";
 
 export function Project() {
-  const { projects, projectId } = useLoaderData({ from: "/app/project/" });
-  const navigate = useNavigate();
-
-  const updateProjectMutation = useChangeProject();
-
-  const handleChangeProject = async (selectedProjectId: string) => {
-    updateProjectMutation.mutateAsync(selectedProjectId, {
-      onSuccess: () => {
-        navigate({
-          to: "/app/project",
-          reloadDocument: true,
-        });
-      },
-    });
-  };
+  const { projects } = useLoaderData({ from: "/app/project/" });
 
   return (
     <div className="grid gap-4">
       <p className="text-2xl font-bold">Projects</p>
       <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
         {projects?.map((project) => {
-          return (
-            <Card
-              key={project.id}
-              onClick={() => handleChangeProject(project.id.toString())}
-              className={cn(
-                "hover:shadow-lg transition-shadow duration-100 cursor-pointer relative",
-                {
-                  "border border-green-200 bg-green-50": project.id === Number(projectId),
-                },
-              )}
-            >
-              {updateProjectMutation.isPending && (
-                <div className="absolute inset-0 bg-black/10 rounded-lg flex items-center justify-center z-10">
-                  <div className="w-6 h-6 border-2 border-blue-600 border-t-transparent rounded-full animate-spin" />
-                </div>
-              )}
-              <CardHeader>
-                <CardTitle>{project.name}</CardTitle>
-                <CardDescription>{project.description}</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Waiting Tasks: 0</p>
-                <p>In Progress Tasks: 0</p>
-                <p>Completed Tasks: 0</p>
-              </CardContent>
-            </Card>
-          );
+          return <ProjectCard key={project.id} project={project} />;
         })}
       </div>
     </div>
---
+++ src/routes/app/project/-project/project-card.tsx
@@ -0,0 +1,55 @@
+import { useLoaderData, useNavigate } from "@tanstack/react-router";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import type { ProjectType } from "@/lib/db/schema";
+import { useChangeProject } from "@/lib/queries/projects";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  project: ProjectType;
+};
+
+export function ProjectCard(props: Props) {
+  const { project } = props;
+  const { projectId } = useLoaderData({ from: "/app/project/" });
+  const navigate = useNavigate();
+  const updateProjectMutation = useChangeProject();
+
+  const handleChangeProject = async (selectedProjectId: string) => {
+    updateProjectMutation.mutateAsync(selectedProjectId, {
+      onSuccess: () => {
+        navigate({
+          to: "/app/project",
+          reloadDocument: true,
+        });
+      },
+    });
+  };
+
+  const isLoading = updateProjectMutation.isPending;
+
+  return (
+    <Card
+      key={project.id}
+      onClick={() => handleChangeProject(project.id.toString())}
+      className={cn("hover:shadow-lg transition-shadow duration-100 cursor-pointer relative", {
+        "border border-green-200 bg-green-50": project.id === Number(projectId),
+        "animate-pulse": isLoading,
+      })}
+    >
+      {updateProjectMutation.isPending && (
+        <div className="absolute inset-0 bg-black/10 rounded-lg flex items-center justify-center z-10">
+          <div className="w-6 h-6 border-2 border-blue-600 border-t-transparent rounded-full animate-spin" />
+        </div>
+      )}
+      <CardHeader>
+        <CardTitle>{project.name}</CardTitle>
+        <CardDescription>{project.description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Waiting Tasks: 0</p>
+        <p>In Progress Tasks: 0</p>
+        <p>Completed Tasks: 0</p>
+      </CardContent>
+    </Card>
+  );
+}
